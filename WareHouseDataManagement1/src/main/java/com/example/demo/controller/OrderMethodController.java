0a564dca78128cabb419b7399de698f96a079fbd
shankar <shankar@DESKTOP-CIQ36D9>
2020-02-09 10:29:45 +0530

    changed return type of save method in orderMethod controller class.

--- WareHouseDataManagement1/src/main/java/com/example/demo/controller/OrderMethodController.java
+++ WareHouseDataManagement1/src/main/java/com/example/demo/controller/OrderMethodController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,16 @@ public class OrderMethodController {
 	@Autowired
 	private IOrderMethodService service;
 	@PostMapping("/save")
-	public Integer saveOrderMethod(@RequestBody OrderMethod orderMethod) {
+	public ResponseEntity<?> saveOrderMethod(@RequestBody OrderMethod orderMethod) {
+		ResponseEntity<?> response=null;
+		try {
+			service.saveOrderMethod(orderMethod);
+			response=new ResponseEntity<String>("saved With"+orderMethod.getId(),HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			response=new ResponseEntity<String>("Data Not saved"+e.getMessage(),HttpStatus.BAD_REQUEST);
+		}
 		
-		service.saveOrderMethod(orderMethod);
-		return orderMethod.getId();
+		return response;
 	}
 }
